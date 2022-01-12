7823afb899f4735dc8e5ce97e933a69f59aaca11
HAMMER17 <plutalov70gmail.com>
2022-01-12 12:00:38 +0300

    valid

---
+++ myreact/src/About/About.js
@@ -0,0 +1,17 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Form, Row, Button, Col, InputGroup, FormExample } from 'react-bootstrap'
+// import FormExampl from './F1'
+import React, { Component } from 'react'
+
+export default class About extends Component {
+
+    render() {
+
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
